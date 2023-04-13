f3ab94c9e62d142a994d2bc6965905b8cb2ab055
pendragons-code <pendragonscode@gmail.com>
2023-04-14 04:44:52 +0800

    Banana time

---
+++ src/buttons/botsite.js
@@ -0,0 +1,12 @@
+const { EmbedBuiler } = require("discord.js")
+const { Bot, Default } = require("../../configs/config.json")
+module.exports = async ({ bot, interactionCreate })=> {
+	const embed = new EmbedBuiler()
+	embed.setColor(Default.DefaultEmbedColor)
+	embed.setFooter({ text: Default.DefaultFooterText })
+	embed.setDescription("Please add the discord bot!")
+	embed.setTitle("Add link!")
+	embed.setURL(Bot.Site)
+}
+// To update all emotes into a json file! This is because the emoji.json fileis outdated!!
+// Nanako will not have heavy implementation of the interactions system
