9d5a0e31070b435e41751437e993d9a1891bd39b
pendragons-code <pendragonscode@gmail.com>
2023-04-16 00:56:57 +0800

    Banana time

--- src/loaders/frontEnd.js
+++ src/loaders/frontEnd.js
@@ -3,3 +3,11 @@ const express = require("express")
 const routeFrontEnd = express.Router()
 const { rateLimit } = require("express-rate-limit")
 const { windowMinutes, maxWindowRequest, standardHeaders, legacyHeaders, message } = require("../../configs/rateLimit.json")
+
+const limiter = rateLimit({
+	windowMs: windowMinutes * 60000,
+	max: maxWindowRequest,
+	standardHeaders: standardHeaders,
+	legacyHeaders: legacyHeaders,
+	message: message
+})
